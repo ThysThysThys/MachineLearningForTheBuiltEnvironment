8f855fe9f260e1e8f33f4e190a15dcbce6cb2025
LarsvanDijk2003 <larsvandijk18@gmail.com>
2026-03-05 00:25:50 +0100

    bug fixes

--- assignment1/constant_velocity_and_acceleration.py
+++ assignment1/constant_velocity_and_acceleration.py
@@ -10,15 +10,15 @@ ts = range(1, 7)
 # running params
 plot_seventh_point = True
 do_constant_v = True
-do_constant_a = False
+do_constant_a = True
 
 # parameters
 # constant v
-iterations_constant_v = [100, 500, 1000, 10000]
-learning_rate_constant_v = [0.1, 0.01, 0.001, 0.0001]
+iterations_constant_v = [100, 500, 1000, 750, 10000, 100000]
+learning_rate_constant_v = [0.1, 0.001, 0.01, 0.0001]
 # constant a
-iterations_constant_a = [10000]
-learning_rate_constant_a = [0.0001]
+iterations_constant_a = [100000, 200000, 500000]
+learning_rate_constant_a = [0.001,  0.0001, 0.00001, 0.000001]
 tolerance = 0.0000001
 initial_v = 1
 initial_b = 0
@@ -77,16 +77,19 @@ def gradient_func_constant_speed_b(ps, ts, v, b):
 def gradient_descent_axis_constant(max_iterations, learning_rate, initial_v, initial_b, ps, ts):
     v = initial_v
     b = initial_b
+    converged_at = None
     for i in range(max_iterations):
         gradient_v = gradient_func_constant_speed_v(ps, ts, v, b)
         gradient_b = gradient_func_constant_speed_b(ps, ts, v, b)
         if abs(gradient_b * learning_rate) < tolerance and abs(gradient_v * learning_rate) < tolerance:
+            converged_at = i
             break
         if abs(gradient_v * learning_rate) > tolerance:
             v = v - learning_rate * gradient_v
         if abs(gradient_b * learning_rate) > tolerance:
             b = b - learning_rate * gradient_b
-    return v, b
+
+    return v, b, converged_at
 
 
 
@@ -126,6 +129,7 @@ def gradient_descent_axis_quadratic(max_iterations, learning_rate, initial_a, in
     a = initial_a
     v = initial_v
     b = initial_b
+    converged_at = None
 
     for i in range(max_iterations):
         ga = gradient_func_constant_acc_a(ps, ts, a, v, b)
@@ -135,13 +139,14 @@ def gradient_descent_axis_quadratic(max_iterations, learning_rate, initial_a, in
         if (abs(ga * learning_rate) < tolerance and
             abs(gv * learning_rate) < tolerance and
             abs(gb * learning_rate) < tolerance):
+            converged_at = i
             break
 
         a = a - learning_rate * ga
         v = v - learning_rate * gv
         b = b - learning_rate * gb
 
-    return a, v, b
+    return a, v, b, converged_at
 
 if __name__ == '__main__':
     if do_constant_v:
@@ -154,14 +159,18 @@ if __name__ == '__main__':
         best_bs = []
         best_iterations = -1
         best_learning_rate = -1
-
+        converged_at_x = None
+        converged_at_y = None
+        converged_at_z = None
+        iterations_constant_v.sort()
+        learning_rate_constant_v.sort()
         # gradient descent for constant velocity and plotting results
         for iterations in iterations_constant_v:
             for learning_rate in learning_rate_constant_v:
                 # do gradient descent
-                vx, bx = gradient_descent_axis_constant(iterations, learning_rate, initial_v, initial_b, pxs, ts)
-                vy, by = gradient_descent_axis_constant(iterations, learning_rate, initial_v, initial_b, pys, ts)
-                vz, bz = gradient_descent_axis_constant(iterations, learning_rate, initial_v, initial_b, pzs, ts)
+                vx, bx, converged_at_x = gradient_descent_axis_constant(iterations, learning_rate, initial_v, initial_b, pxs, ts)
+                vy, by, converged_at_y = gradient_descent_axis_constant(iterations, learning_rate, initial_v, initial_b, pys, ts)
+                vz, bz, converged_at_z = gradient_descent_axis_constant(iterations, learning_rate, initial_v, initial_b, pzs, ts)
 
                 # calculate xyzs and determine SSE
                 x=[]
@@ -184,8 +193,11 @@ if __name__ == '__main__':
                     best_bs = [bx, by, bz]
                     best_iterations = iterations
                     best_learning_rate = learning_rate
+                    if converged_at_x is not None and converged_at_y is not None and converged_at_z is not None:
+                        iterations_needed = max(converged_at_x, converged_at_y, converged_at_z)
 
-        print(f"For constant velocity:\nBest error: {best_error}\nBest number of iterations: {best_iterations}\nBest learning rate: {best_learning_rate}\nV values per axis: {best_vs}\nB values per axis: {best_bs}\n")
+
+        print(f"For constant velocity:\nBest error: {best_error}\nBest max iterations: {best_iterations}, {iterations_needed} needed to converge \nBest learning rate: {best_learning_rate}\nv values per axis: {best_vs}\nb values per axis: {best_bs}\n")
         Plotter.plotter(best_xs, best_ys, best_zs, ts)
 
     if do_constant_a:
@@ -197,20 +209,27 @@ if __name__ == '__main__':
         best_as = []
         best_vs = []
         best_bs = []
+        x7 = None
+        y7 = None
+        z7 = None
         best_iterations = -1
         best_learning_rate = -1
-
+        converged_at_x = None
+        converged_at_y = None
+        converged_at_z = None
+        iterations_constant_a.sort()
+        learning_rate_constant_a.sort()
         # gradient descent for constant acceleration and plotting results
         for iterations in iterations_constant_a:
             for learning_rate in learning_rate_constant_a:
                 # do gradient descent
-                ax, vx, bx = gradient_descent_axis_quadratic(
+                ax, vx, bx, converged_at_x = gradient_descent_axis_quadratic(
                     iterations, learning_rate, initial_a, initial_v, initial_b, pxs, ts
                 )
-                ay, vy, by = gradient_descent_axis_quadratic(
+                ay, vy, by, converged_at_y = gradient_descent_axis_quadratic(
                     iterations, learning_rate, initial_a, initial_v, initial_b, pys, ts
                 )
-                az, vz, bz = gradient_descent_axis_quadratic(
+                az, vz, bz, converged_at_z = gradient_descent_axis_quadratic(
                     iterations, learning_rate, initial_a, initial_v, initial_b, pzs, ts
                 )
 
@@ -223,31 +242,36 @@ if __name__ == '__main__':
                     z.append(func_constant_acc(az, i, vz, bz))
                 total_error = error_func_constant_acc(pxs, ts, ax, vx, bx) + error_func_constant_acc(pys, ts, ay, vy, by) + error_func_constant_acc(pzs, ts, az, vz, bz)
 
-                #print(f"total error {total_error} for {iterations} iterations and {learning_rate} learning rate")
+                # print(f"total error {total_error} for {iterations} iterations and {learning_rate} learning rate")
 
-                if plot_seventh_point:
-                    t_next = 7
 
-                    x7 = func_constant_acc(ax, t_next, vx, bx)
-                    y7 = func_constant_acc(ay, t_next, vy, by)
-                    z7 = func_constant_acc(az, t_next, vz, bz)
-
-                    x.append(x7)
-                    y.append(y7)
-                    z.append(z7)
 
                 if total_error < best_error:
-                    best_error = total_error
-                    best_xs = x
-                    best_ys = y
-                    best_zs = z
-                    best_as = [ax, ay, az]
-                    best_vs = [vx, vy, vz]
-                    best_bs = [bx, by, bz]
-                    best_iterations = iterations
-                    best_learning_rate = learning_rate
+                    if converged_at_x is not None and converged_at_y is not None and converged_at_z is not None:
+                        iterations_needed = max(converged_at_x, converged_at_y, converged_at_z)
+                        best_error = total_error
+                        best_xs = x
+                        best_ys = y
+                        best_zs = z
+                        best_as = [ax, ay, az]
+                        best_vs = [vx, vy, vz]
+                        best_bs = [bx, by, bz]
+                        best_iterations = iterations
+                        best_learning_rate = learning_rate
 
-        print(f"For constant acceleration:\nBest error: {best_error}\nBest number of iterations: {iterations}\nBest learning rate: {learning_rate}\nA values per axis: {best_as}\nV values per axis: {best_vs}\nB values per axis: {best_bs}")
+                    if plot_seventh_point:
+                        t_next = 7
+
+                        x7 = func_constant_acc(ax, t_next, vx, bx)
+                        y7 = func_constant_acc(ay, t_next, vy, by)
+                        z7 = func_constant_acc(az, t_next, vz, bz)
+
+                        x.append(x7)
+                        y.append(y7)
+                        z.append(z7)
+
+        print(f"For constant acceleration:\nBest error: {best_error}\nBest max iterations: {best_iterations}, {iterations_needed} needed to converge \nBest learning rate: {best_learning_rate}\na values per axis: {best_as}\nv values per axis: {best_vs}\nb values per axis: {best_bs}\n")
         if plot_seventh_point:
             ts = range(1, 8)
+            print(f"Most likely position drone at t=7: ({x7}, {y7}, {z7})")
         Plotter.plotter(best_xs, best_ys, best_zs, ts)
