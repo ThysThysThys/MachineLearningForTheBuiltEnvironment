7fe1e10073d6aed11157ef108411584d2c1edc5c
Thys Kok <T.A.Kok@student.tudelft.nl>
2026-04-07 10:50:21 +0200

    update main

--- GEO5017-A3-UrbanWaste/main.py
+++ GEO5017-A3-UrbanWaste/main.py
@@ -1,6 +1,33 @@
 from ultralytics import YOLO
+import os
 
+# params
 model_type = "yolo26n.pt"
+model_path_output = "models/trained_models"
+model_path = "../runs/detect/"
+num_epochs = 25
+batch_size =16
 
-model = YOLO(model_type)
-results = model.train()
+def train():
+    print("starting training")
+    model = YOLO(model_type)
+    model.train(data="Annoted_Data/annoted_dataset_yolo/data.yaml", epochs=num_epochs, batch=batch_size, project=model_path_output, exist_ok=True)
+
+    print("starting validation")
+    model.val()
+
+def test():
+    model = YOLO(model_path + model_path_output + "/train/weights/best.pt")
+    print("starting testing")
+    results = model.predict("Test_data_small")
+    results.sort(key= lambda r: 0 if r.probs is None else r.probs.top1conf[0].item(), reverse=True)
+    results = results[:100]
+    for result in results:
+        print("None" if result.probs is None else result.probs.top1conf)
+        result.save()
+
+if __name__ == '__main__':
+    if not os.path.exists(model_path + model_path_output):
+        print("need to train model")
+        train()
+    test()
